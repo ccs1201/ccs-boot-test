7a4970b2f836d2b37f38d2d78682f58f8eff9706
Cleber.Ssouza <ccs1201@gmail.com>
2025-07-15 03:45:04 -0300

    Implementação com ccs-boot

--- src/main/java/br/com/testes/ccsboot/CcsBootTestApplication.java
+++ src/main/java/br/com/testes/ccsboot/CcsBootTestApplication.java
@@ -2,9 +2,9 @@ package br.com.testes.ccsboot;
 
 import br.com.ccs.boot.CcsBootApplication;
 
-public class App {
+public class CcsBootTestApplication {
 
     public static void main(String[] args) {
-        CcsBootApplication.run(App.class, 8080, "/");
+        CcsBootApplication.run(CcsBootTestApplication.class, 8080, "/");
     }
 }
